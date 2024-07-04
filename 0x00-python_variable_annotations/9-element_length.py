01e2a746794003a161b7fcc0a44ff0e488b7de02
kok <kok@ubuntu.ubuntu-domain>
2024-07-04 10:50:19 -0700

    added docstring

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-'''annotated func'''
+'''annotated functions'''
 from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    '''....'''
+    '''return values with the appropriate types'''
     return [(i, len(i)) for i in lst]
