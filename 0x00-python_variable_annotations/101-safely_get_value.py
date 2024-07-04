e5d74527738da52d93293257b3078408543b44ac
kok <kok@ubuntu.ubuntu-domain>
2024-07-04 10:05:32 -0700

    fixed output to match req

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-'''....'''
+'''function'''
 from typing import Sequence, Any, Mapping, Optional, TypeVar, Union
 
 T = TypeVar('T')
 
 
 def safely_get_value(
-    dct: Mapping[Any, T],
+    dct: Mapping,
     key: Any,
     default: Union[T, None] = None
-) -> Union[T, None]:
+) -> Union[Any, T]:
     '''....'''
     if key in dct:
         return dct[key]
