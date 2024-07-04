70b819558d4878729988b65d26b7afd32ee6b2e7
kok <kok@ubuntu.ubuntu-domain>
2024-07-04 10:52:00 -0700

    added docstring

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-'''....'''
+'''functions'''
 from typing import Sequence, Optional, Any
 
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    '''add type annotations to the function'''
     if lst:
         return lst[0]
     else:
