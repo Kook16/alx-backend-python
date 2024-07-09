b3816f9da6be2284b22c8a23af2c030b6582616b
kok <kok@ubuntu.ubuntu-domain>
2024-07-09 02:42:07 -0700

    applied type annotations

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -5,12 +5,10 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     '''measure the total runtime and return it'''
     start_time = time.perf_counter()
-
     # Run async_comprehension four times in parallel
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
-
     end_time = time.perf_counter()
     return end_time - start_time
